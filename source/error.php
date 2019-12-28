de090b684cdf4d27b10b9af5fc96374645b52522
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2019-12-28 16:15:42 +0700

    Create error.php

---
+++ source/error.php
@@ -0,0 +1,10 @@
+<div class="row justify-content-center">
+    <div class="jumbotron bg-dark">
+        <h1 class="display-4 text-danger"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+            404</h1>
+        <p class="lead text-white">Halaman yang anda cari tidak ada.</p>
+        <hr class="my-4">
+        <p class="text-white">Silahkan balik ke Homepage.</p>
+        <a class="btn btn-danger btn-lg" href="index.php?halaman=home" role="button">Homepage</a>
+    </div>
+</div>
