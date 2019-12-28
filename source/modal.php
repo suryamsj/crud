f4b6faa853d01b20905634ca9c9618f72d6c6ce1
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2019-12-28 16:16:15 +0700

    Create modal.php

---
+++ source/modal.php
@@ -0,0 +1,25 @@
+<!-- Modal -->
+<div class="modal fade" id="tentang" tabindex="-1" role="dialog" aria-labelledby="tentangTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark text-white">
+                <h5 class="modal-title" id="tentangTitle">Tugas Praktikum Basis Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span class="text-white" aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Tugas ini dibuat oleh : <br>
+                <img class="emojika" alt="man" src="asset/img/man.png" />&nbsp;Muhammad Surya Jayadiprana | <b>18107040</b><br>
+                <img class="emojika" alt="girl" src="asset/img/girl.png" />&nbsp;Ince Clara | <b>18107040</b><br>
+                <img class="emojika" alt="girl" src="asset/img/girl.png" />&nbsp;Cindy Natasya | <b>18107040</b><br>
+                <img class="emojika" alt="girl" src="asset/img/girl.png" />&nbsp;Fitra Fatimah Putri | <b>18107040</b><br>
+                <img class="emojika" alt="girl" src="asset/img/girl.png" />&nbsp;Sherina | <b>18107040</b><br>
+                <p>Pengambilan Kasus : <b><i>Matakuliah</i></b></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
